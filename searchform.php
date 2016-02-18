b301372b82354cb217e0534e3f1ef69a19e18a7c
hendratno <hendratgiovani@gmail.com>
2016-02-18 08:07:48 +0700

    Added style on sidebar

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
     class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div>
         <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+        <input type="text"  ="<?php echo get_search_query(); ?>" name="s" id="s" />
         <input type="submit" id="searchsubmit"
             value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
     </div>
